2acdb5ad6e66b324883d04df50a103c99be9676d
gabogara <gabo1gara@gmail.com>
2026-04-14 09:18:33 -0700

    Create initial Express app with root route

---
+++ assignment2/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  console.log(`Server is listening on port ${port}...`)
+);
+
+module.exports = { app, server };
