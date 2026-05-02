b445f19ad28fcaf8752b93f147468eb6f66f5a39
gabogara <gabo1gara@gmail.com>
2026-05-01 23:51:19 -0700

    refactor: move password hashing helpers to utils

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const { userSchema } = require("../validation/userSchema");
-const crypto = require("crypto");
-const util = require("util");
-
-const scrypt = util.promisify(crypto.scrypt);
-
-async function hashPassword(password) {
-  const salt = crypto.randomBytes(16).toString("hex");
-  const derivedKey = await scrypt(password, salt, 64);
-  return `${salt}:${derivedKey.toString("hex")}`;
-}
-
-async function comparePassword(inputPassword, storedHash) {
-  const [salt, key] = storedHash.split(":");
-  const keyBuffer = Buffer.from(key, "hex");
-  const derivedKey = await scrypt(inputPassword, salt, 64);
-  return crypto.timingSafeEqual(keyBuffer, derivedKey);
-}
+const { hashPassword, comparePassword } = require("../utils/passwordUtils");
 
 const register = async (req, res) => {
   if (!req.body) req.body = {};
