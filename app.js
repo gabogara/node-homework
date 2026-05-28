d8e89831c8ed3c8170659d0fe02ec147c6de94c5
gabogara <gabo1gara@gmail.com>
2026-05-28 13:23:02 -0700

    feat: add security middleware

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ const taskRouter = require("./routes/taskRoutes");
 const prisma = require("./db/prisma");
 const analyticsRouter = require("./routes/analyticsRoutes");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimiter = require("express-rate-limit");
 
 const app = express();
+app.set("trust proxy", 1);
 global.user_id = null;
 global.users = [];
 global.tasks = [];
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
+app.use(helmet());
 app.use(express.json({ limit: "1kb" }));
 app.use(cookieParser());
+app.use(xss());
 
 app.use((req, res, next) => {
   console.log(req.method, req.path, req.query);
