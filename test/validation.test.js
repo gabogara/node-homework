54fb724c01d1c2ff53c3de3a7d509d0b1d086493
gabogara <gabo1gara@gmail.com>
2026-06-02 14:21:06 -0700

    test: add user schema validation tests

--- test/validation.test.js
+++ test/validation.test.js
@@ -13,3 +13,67 @@ describe("user object validation tests", () => {
     ).toBeDefined();
   });
 });
+
+it("2. The user schema requires that an email be specified.", () => {
+  const { error } = userSchema.validate(
+    { name: "Bob", password: "Pa$$word20" },
+    { abortEarly: false }
+  );
+
+  expect(
+    error.details.find((detail) => detail.context.key === "email")
+  ).toBeDefined();
+});
+
+it("3. The user schema does not accept an invalid email.", () => {
+  const { error } = userSchema.validate(
+    { name: "Bob", email: "not-an-email", password: "Pa$$word20" },
+    { abortEarly: false }
+  );
+
+  expect(
+    error.details.find((detail) => detail.context.key === "email")
+  ).toBeDefined();
+});
+
+it("4. The user schema requires a password.", () => {
+  const { error } = userSchema.validate(
+    { name: "Bob", email: "bob@sample.com" },
+    { abortEarly: false }
+  );
+
+  expect(
+    error.details.find((detail) => detail.context.key === "password")
+  ).toBeDefined();
+});
+
+it("5. The user schema requires name.", () => {
+  const { error } = userSchema.validate(
+    { email: "bob@sample.com", password: "Pa$$word20" },
+    { abortEarly: false }
+  );
+
+  expect(
+    error.details.find((detail) => detail.context.key === "name")
+  ).toBeDefined();
+});
+
+it("6. The name must be valid (3 to 30 characters).", () => {
+  const { error } = userSchema.validate(
+    { name: "Bo", email: "bob@sample.com", password: "Pa$$word20" },
+    { abortEarly: false }
+  );
+
+  expect(
+    error.details.find((detail) => detail.context.key === "name")
+  ).toBeDefined();
+});
+
+it("7. If validation is performed on a valid user object, error comes back falsy.", () => {
+  const { error } = userSchema.validate(
+    { name: "Bob", email: "bob@sample.com", password: "Pa$$word20" },
+    { abortEarly: false }
+  );
+
+  expect(error).toBeFalsy();
+});
