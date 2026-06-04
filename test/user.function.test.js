756895492a4bd954dc82e7ca152487bb119b278c
gabogara <gabo1gara@gmail.com>
2026-06-03 17:00:42 -0700

    test: add network registration test

---
+++ test/user.function.test.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+
+const request = require("supertest");
+const prisma = require("../db/prisma");
+
+const { app, server } = require("../app");
+
+let agent;
+let saveRes;
+
+beforeAll(async () => {
+  await prisma.task.deleteMany();
+  await prisma.user.deleteMany();
+
+  agent = request.agent(app);
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+  server.close();
+});
+
+describe("testing actual network operations", () => {
+  it("46. it creates the user entry", async () => {
+    const newUser = {
+      name: "John Deere",
+      email: "jdeere@example.com",
+      password: "Pa$$word20",
+    };
+
+    saveRes = await agent.post("/api/users/register").send(newUser);
+
+    expect(saveRes.status).toBe(201);
+  });
+});
