704eebb5d30bfbe1380245d503294fd68cd27edf
gabogara <gabo1gara@gmail.com>
2026-06-03 21:31:52 -0700

    test: add network logout verification test

--- test/user.function.test.js
+++ test/user.function.test.js
@@ -68,3 +68,9 @@ it("51. Verify that you can log out.", async () => {
 
   expect(saveRes.status).toBe(200);
 });
+
+it("52. Make sure that you are really logged out: /api/tasks should now return a 401.", async () => {
+  saveRes = await agent.get("/api/tasks");
+
+  expect(saveRes.status).toBe(401);
+});
